1ad6f4f37b47d6c7fed2281593305daa8560b46e
WIZZYCRYPT <realwizzycrypt@gmail.com>
2025-02-08 18:46:43 +0100

    Update FundMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -39,8 +39,40 @@ contract FundMe {
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    receive() external payable { 
+        fund(); 
+    }
+
+    fallback() external payable { 
+        fund(); 
+    }
 }
-
-
-
-
