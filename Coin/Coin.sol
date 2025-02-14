6cc5f47fd777ec3d04d3b11d4fad31b5edc5e95f
WIZZYCRYPT <realwizzycrypt@gmail.com>
2025-02-14 20:11:32 +0100

    Update Coin.sol

--- Coin/Coin.sol
+++ Coin/Coin.sol
@@ -1,6 +1,33 @@
+// DESCRIPTION: A simple contract allows to owner to create a coin,...
+// ...and send some amount of the coin to an address upon creation....
+// ...Contract also allows for addresses to send coins which were...
+//... sent to them by the contract owner or other addresses
+
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.26;
 
 contract Coin {
 
     address public minter;
     mapping(address => uint) public balances;
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    // Sends an amount of newly created coins to an address
+    // Can only be called by the contract creator
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+    // Sends an amount of existing coins
+    // from any caller to an address
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+}
